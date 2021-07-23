df2266ab82c09b139dd7d130f1660370a9e2b62e
KingMabuza <amal@amallevi.com>
2021-07-23 16:20:59 +0200

    first commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="static/style.css"/>
-    <limk href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
     <title>Welcome | Jamii</title>
 </head>
 <body>
